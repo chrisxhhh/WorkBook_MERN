d4a836b0ce798777672ae7bdf6e05fb3dd8e5810
Chris Xu <chrisxu2017@gmail.com>
2022-08-29 21:08:49 -0400

    deploy test 05

--- server.js
+++ server.js
@@ -20,12 +20,19 @@ app.use('/api/worker', workerRoute);
 const port = process.env.PORT || 5000;
 
 
-if (process.env.NODE_ENV ==='production'){
-    app.use('/',express.static('client/build'));
-    app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-    });
-}
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
-app.get("/", (req,res)=> res.send("hello world"));
-app.listen(port, ()=>console.log(`Node server started at port ${port}`)); 
+// if (process.env.NODE_ENV ==='production'){
+//     app.use('/',express.static('client/build'));
+//     app.get('*', (req,res)=>{
+//         res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+//     });
+// }
+
+//app.get("/", (req,res)=> res.send("hello world"));
+
+
+app.listen(port, () => console.log(`Node server started at port ${port}`)); 
